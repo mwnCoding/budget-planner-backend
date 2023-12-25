2afe549495a36683cad62baedc3e498945cf6d85
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-12-25 16:27:47 -0500

    Implement income routes with model

--- routes/incomes.js
+++ routes/incomes.js
@@ -1,38 +1,68 @@
 const express = require("express");
 const router = express.Router();
 
+const Income = require("../models/income");
+
 //Get all incomes
-router.get("/", (req, res) => {
-    res.send("Get all incomes");
+router.get("/", async (req, res) => {
+    try {
+        const incomes = await Income.find({});
+        res.send(incomes);
+    } catch {
+        res.send("Error getting all incomes");
+    }
 });
 
 //Get income with specific ID
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const id = req.params.id;
-    res.send(`Get income with the id ${id}`);
+    try {
+        const income = await Income.findById(id);
+        res.send(income);
+    } catch {
+        res.send("Error getting specified income");
+    }
 });
 
 //Create new income
-router.post("/new", (req, res) => {
-    const newIncome = {
+router.post("/new", async (req, res) => {
+    const newIncome = new Income({
         value: req.body.value,
         type: req.body.type,
-        description: req.body.type
+        description: req.body.description
+    })
+    try {
+        const savedIncome = await newIncome.save();
+        res.send("Income sucessfully created");
+    } catch {
+        res.send("Error creating new income");
     }
-    console.log(newIncome);
-    res.send("Created new income");
 });
 
 //Delete income
-router.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
     const id = req.params.id;
-    res.send(`Deleted income with the ID ${id}`);
+    try {
+        const deletedIncome = await Income.findByIdAndDelete(id);
+        res.send("Income deleted succesfully");
+    } catch {
+        res.send("Error deleting income");
+    }
 });
 
 //Update income with specific ID
-router.patch("/update/:id", (req, res) => {
+router.patch("/update/:id", async (req, res) => {
     const id = req.params.id;
-    res.send(`Updated inome with the ID ${id}`);
+    try {
+        const updatedIncome = await Income.findByIdAndUpdate(id, {
+            value: req.body.value,
+            type: req.body.type,
+            description: req.body.description
+        });
+        res.send("Income updated succesfully")
+    } catch {
+        res.send("Error updating income");
+    }
 });
 
 module.exports = router;
